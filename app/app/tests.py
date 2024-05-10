b6bf40e4fbf59404bc9a19a2f2b6f03e7284891f
Fredrik Strandberg <91421729+therealfigge@users.noreply.github.com>
2024-05-10 13:00:11 +0200

    Fixed linting issues in unittest files again.

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,13 +4,13 @@ from django.test import SimpleTestCase
 from app import calc 
 
 class CalcTest(SimpleTestCase):
-    
+
     def test_add_numbers(self):
         res = calc.add(5, 6)
-        
-        self.assertEqual(res, 11)
     
+        self.assertEqual(res, 11)
+
     def test_subtract(self):
         res = calc.subtract(10, 15)
-        
+    
         self.assertEqual(res, 5)
