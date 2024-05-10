dbb5c38ae5744fcea9a9c4f31f2804af566ab3cb
Fredrik Strandberg <91421729+therealfigge@users.noreply.github.com>
2024-05-10 12:55:17 +0200

    Fixed linting issues in unittest files.

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,5 +5,7 @@ def add(x, y):
     """ Return sum of x + y"""
     return x + y
 
+
 def subtract(x, y):
     return y - x
+
--- app/app/tests.py
+++ app/app/tests.py
@@ -9,8 +9,9 @@ class CalcTest(SimpleTestCase):
         res = calc.add(5, 6)
         
         self.assertEqual(res, 11)
-        
+    
     def test_subtract(self):
         res = calc.subtract(10, 15)
         
         self.assertEqual(res, 5)
+        
