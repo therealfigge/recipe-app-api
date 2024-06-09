11ac633ba12c1cac97f6fa66d929c032d069aacb
Fredrik Strandberg <91421729+therealfigge@users.noreply.github.com>
2024-06-09 03:07:36 +0200

    Implementation of API for listing recipes.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -25,7 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         """Update and return user."""
         password = validated_data.pop('password', None)
-        user = super().update(instance. validated_data)
+        user = super().update(instance, validated_data)
 
         if password:
             user.set_password(password)
--- app/user/views.py
+++ app/user/views.py
@@ -24,7 +24,7 @@ class CreateTokenView(ObtainAuthToken):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
-    serializer_classes = UserSerializer
+    serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
